f333ce84e262f0ccc9082f76370e5dd921c0425a
jose-luis-dev <luiskrad@gmail.com>
2023-08-23 14:55:41 -0600

    Se actualiza las API

--- service/clientService.js
+++ service/clientService.js
@@ -1,59 +1,57 @@
-const listaUsuarios =() => fetch('http://localhost:3000/usuarios').then((respuesta) => respuesta.json());
-const listaProductos =() => fetch('http://localhost:3000/productos').then((respuesta) => respuesta.json());
-
-
+const listaUsuarios = () => fetch('https://my-json-server.typicode.com/jose-luis-dev/E-commerce/usuarios').then((respuesta) => respuesta.json());
+const listaProductos = () => fetch('https://my-json-server.typicode.com/jose-luis-dev/E-commerce/productos').then((respuesta) => respuesta.json());
 
 const login = async (email, contrasena) => {
-    const respuesta = await fetch('http://localhost:3000/usuarios');
+    const respuesta = await fetch('https://my-json-server.typicode.com/jose-luis-dev/E-commerce/usuarios');
     const usuarios = await respuesta.json();
     const usuarioEncontrado = usuarios.find((usuario) => usuario.email === email && usuario.contrasena === contrasena);
     return { autenticado: !!usuarioEncontrado };
 };
 
-const crearProducto = (nombre, precio, imagen, categoria, descripcion) =>{
-    return fetch('http://localhost:3000/productos', {
+const crearProducto = (nombre, precio, imagen, categoria, descripcion) => {
+    return fetch('https://my-json-server.typicode.com/jose-luis-dev/E-commerce/productos', {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ nombre, precio, imagen, categoria, descripcion, id: uuid.v4() }),
+        body: JSON.stringify({ nombre, precio, imagen, categoria, descripcion }),
     });
 };
 
-const crearUsuario =(email, contrasena) => {
-    return fetch('http://localhost:3000/productos', {
+const crearUsuario = (email, contrasena) => {
+    return fetch('https://my-json-server.typicode.com/jose-luis-dev/E-commerce/usuarios', {
         method: "POST",
-        headers:{
+        headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({email, contrasena, id: uuid.v4() }),
+        body: JSON.stringify({ email, contrasena }),
     });
 };
 
 const detalleProducto = (id) => {
-    return fetch(`http://localhost:3000/productos/${id}`).then((respuesta) => respuesta.json());
+    return fetch(`https://my-json-server.typicode.com/jose-luis-dev/E-commerce/productos/${id}`).then((respuesta) => respuesta.json());
 };
 
 const actualizarProducto = (nombre, precio, imagen, categoria, descripcion, id) => {
-    return fetch(`http://localhost:3000/productos/${id}`, {
+    return fetch(`https://my-json-server.typicode.com/jose-luis-dev/E-commerce/productos/${id}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({nombre, precio, imagen, categoria, descripcion})
+        body: JSON.stringify({ nombre, precio, imagen, categoria, descripcion })
     })
         .then((respuesta) => respuesta)
         .catch(err => console.log(err));
 };
 
 const eliminarProducto = (id) => {
-    return fetch(`http://localhost:3000/productos/${id}`, {
+    return fetch(`https://my-json-server.typicode.com/jose-luis-dev/E-commerce/productos/${id}`, {
         method: 'DELETE'
     });
 };
 
 const verProductoLink = (id) => {
-    return fetch(`http://localhost:3000/productos/${id}`).then((respuesta) => respuesta.json());
+    return fetch(`https://my-json-server.typicode.com/jose-luis-dev/E-commerce/productos/${id}`).then((respuesta) => respuesta.json());
 };
 
 export const clientService = {
