a925f3d129d52ac973d659505408d782dfd4a516
jose-luis-dev <luiskrad@gmail.com>
2023-08-23 15:00:24 -0600

    Se actualiza las API

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ function irLogin(){
 
     btnLogin.addEventListener('click',(e) =>{
         e.preventDefault();
-        window.location.href = '../screens/login.html';
+        window.location.href = '/E-commerce/screens/login.html';
     })
 }
 irLogin();
@@ -26,7 +26,7 @@ function verProductos(){
         verTodos[index].addEventListener('click',(e)=>{
             e.preventDefault();
             window.location.reload();
-            window.location.href = '../screens/productos.html';
+            window.location.href = '/E-commerce/screens/productos.html';
         })
     }
 }
